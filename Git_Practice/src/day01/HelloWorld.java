3d0acc537a0b51d067bc1796a5fdf10f4f304c03
Mia <adi_dinica@yahoo.com>
2022-03-31 23:24:25 +0100

    work 3 is done and about to be pushed to GitHub

--- Git_Practice/src/day01/HelloWorld.java
+++ Git_Practice/src/day01/HelloWorld.java
@@ -7,4 +7,8 @@ public class HelloWorld {
     //doing awesome one too
     // let's do this
 
+    //doing awesome work 3
+    //
+    // Awesome work 3 is done and ready to be committed
+    // and ready to be pushed to remote repository
 }
