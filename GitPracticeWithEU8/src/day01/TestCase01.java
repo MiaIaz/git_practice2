c9c0d13da1962cdad604899314b61636e49a7881
Mia <adi_dinica@yahoo.com>
2022-04-08 08:49:08 +0100

    initial commit

--- GitPracticeWithEU8/src/day01/TestCase01.java
+++
@@ -1,9 +0,0 @@
-package day01;
-
-public class TestCase01 {
-    public static void main(String[] args) {
-
-        System.out.println("Testing with Chrome Browser");
-
-    }
-}
--- GitPracticeWithEU8/src/day02/TestCase02.java
+++
@@ -1,9 +0,0 @@
-package day02;
-
-public class TestCase02 {
-
-    public static void main(String[] args) {
-
-        System.out.println("Amazon log in testes");
-    }
-}
